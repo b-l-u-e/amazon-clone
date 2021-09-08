3560da10b2d861151592aa2d689d78ff6b70d5e7
winniegitau <32414660+b-l-u-e@users.noreply.github.com>
2021-09-08 16:17:41 +0300

    updating the title font size

--- src/components/Product.js
+++ src/components/Product.js
@@ -18,7 +18,7 @@ function Product({ id, title, price, description, category, image }) {
         {category}
       </p>
       <Image src={image} height={200} width={200} objectFit="contain" />
-      <h4 className="my-3">{title}</h4>
+      <h4 className="my-3 md:text-sm">{title}</h4>
       <div className="flex">
         {Array(rating)
           .fill()
